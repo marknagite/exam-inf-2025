81f108ed2439819d1b87657173c54c653998fa76
marknagite <mark.baz@mail.ru>
2026-01-14 01:18:32 +0300

    Create sample_data.sql

---
+++ online-store/sql/sample_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users(name,email,role,password_hash,loyalty_level) VALUES ('Admin','admin@example.com','admin','hash',1);
+INSERT INTO users(name,email,role,password_hash,loyalty_level) VALUES ('Manager','mgr@example.com','manager','hash',0);
+INSERT INTO users(name,email,role,password_hash,loyalty_level) VALUES ('Customer','cust@example.com','customer','hash',0);
+
+INSERT INTO products(name,price,stock_quantity) VALUES ('ProductA',10.5,100);
+INSERT INTO products(name,price,stock_quantity) VALUES ('ProductB',20,50);
